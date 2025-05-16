3cddf1912a7ea507d7f1f6471644efb7d86ca132
Lívia Vilhena <liviavilhena@outlook.com>
2025-05-16 09:43:31 -0300

    Criando 'head' em 'partials'

--- recipes/views.py
+++ recipes/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render (request, 'recipes/home.html')
+    return render (request, 'recipes/pages/home.html')
 
