87496306e6b682505500884de8c4932f4078d54a
livia.vilhena <liviavilhena@outlook.com>
2025-05-19 16:18:08 -0300

    Usando o models Recipe com QuerySet na view home e na view recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 # Create your views here.
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     ctx= {
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+        #[make_recipe() for _ in range(10)],
     }
-    return render (request, 'recipes/pages/home.html', ctx)
+    return render(request, 'recipes/pages/home.html', ctx)
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id= category_id).order_by('-id')
+    ctx= {
+        'recipes': recipes,
+        #[make_recipe() for _ in range(10)],
+    }   
+    return render(request, 'recipes/pages/home.html', ctx)
 
 
 def recipe(request, id):
-    return render (request, 'recipes/pages/recipe-view.html', context= {
-        'recipe': make_recipe(),
+    recipe = Recipe.objects.get(id=id)
+    return render(request, 'recipes/pages/recipe-view.html', context= {
+        'recipe': recipe,
         'is_detail_page': True,
     })
 
