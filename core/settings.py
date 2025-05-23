94059238502f0bc3a8c7f144a9257f6973a4159c
livia.vilhena <liviavilhena@outlook.com>
2025-05-23 15:23:47 -0300

    Correção da variável de ambiente DEBUG no arquivo settings.py e adição de teste de paginação no RecipeHomeViewTest.

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if on.environ.get('DEBUG') == '1' else False
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS = []
 
--- recipes/tests/test_recipe_home_view.py
+++ recipes/tests/test_recipe_home_view.py
@@ -2,7 +2,7 @@ from django.urls import resolve,reverse
 from recipes import views
 from recipes.models import Recipe
 from .test_recipe_base import RescipeTestBase
-
+from unittest.mock import patch
 
 
 class RecipeHomeViewTest(RescipeTestBase):
@@ -24,3 +24,18 @@ class RecipeHomeViewTest(RescipeTestBase):
         Recipe.objects.get(pk=1).delete()
         response =self.client.get(reverse("recipes:home"))
         self.assertIn('<h1>No recipes found here</h1>',response.content.decode('utf-8'))
+
+    def test_recipe_home_is_paginated(self):
+        for i in range(8):
+            kwargs = {'slug': f'r{i}', 'author_data': {'username': f'u{i}'}}
+            self.make_recipe(**kwargs)
+
+        with patch('recipes.views.PER_PAGES', new=4):
+            response = self.client.get(reverse('recipes:home'))
+            recipes = response.context['recipes']
+            paginator = recipes.paginator
+
+            self.assertEqual(paginator.num_pages, 3)
+            self.assertEqual(len(paginator.get_page(1)), 4)
+            self.assertEqual(len(paginator.get_page(2)), 4)
+            self.assertEqual(len(paginator.get_page(3)), 1)
