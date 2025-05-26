7d32c6128d4fd7fa70fb412c0c30edb5dd2cc935
livia.vilhena <liviavilhena@outlook.com>
2025-05-26 10:50:01 -0300

    Adição do formulário de registro e atualização da view para renderizar o formulário na página de registro.

--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .forms import RegisterForm
 
 # Create your views here.
 def register_view(request):
+    form = RegisterForm()
+
+    # context
+    ctx ={
+        'form' : form,
+    }
     return render(request, 'authors/pages/register_view.html')
