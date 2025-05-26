b681ec2e27140bda50cd018c09a738c16fa9e740
livia.vilhena <liviavilhena@outlook.com>
2025-05-26 15:08:54 -0300

    Adição de validação de senha forte no formulário de registro. Implementação de regex para garantir que a senha contenha letras maiúsculas, minúsculas e números, com comprimento mínimo de 8 caracteres.

--- authors/forms.py
+++ authors/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+import re
 
 def add_attr(field, attr_name, attr_new_val):
     existing_attr = field.widget.attrs.get(attr_name, '')
@@ -8,6 +9,21 @@ def add_attr(field, attr_name, attr_new_val):
 
 def add_placeholder(field, placeholder_val):
     add_attr(field, 'placeholder', placeholder_val)
+
+def strong_password(password):
+    # Checagem se o que vem a frente está entre A a Z minusculo e minusculo e entre 0 e 9
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        ),
+            code='invalid'
+        )
+
+
 
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -30,7 +46,8 @@ class RegisterForm(forms.ModelForm):
             'Password must have at least one uppercase letter, '
             'one lowercase letter and one number. The length should be '
             'at least 8 characters.'
-        )
+        ),
+        validators= [strong_password]
     )
 
 
