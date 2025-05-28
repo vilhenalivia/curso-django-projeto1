c678e9eddd6186cbf72cc32c7b1967af9cb2ac4c
livia.vilhena <liviavilhena@outlook.com>
2025-05-28 10:34:15 -0300

    Criação da nova página de edição de receitas e atualização das URLs e lógica de visualização para suportar essa funcionalidade.

--- authors/urls.py
+++ authors/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/create/', views.login_create, name='login_create'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('dashboard/recipe/<int:id>/edit', views.dashboard_recipe_edit, name='dashboard_recipe_edit'),
 ]
