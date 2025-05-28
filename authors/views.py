9732f1a2e8619e7c9c9b10a0c167c78b0d96dfae
livia.vilhena <liviavilhena@outlook.com>
2025-05-28 09:55:58 -0300

    Adição da página de dashboard com mensagem de login e inclusão de novos templates. Atualização da lógica de redirecionamento após login e logout. Melhoria na exibição de receitas no admin.

--- authors/views.py
+++ authors/views.py
@@ -53,7 +53,7 @@ def login_create(request):
         raise Http404()
     
     form = LoginForm(request.POST)
-    login_url = reverse('authors:login')
+    
 
     if form.is_valid():
         authenticated_user = authenticate(
@@ -69,7 +69,7 @@ def login_create(request):
     else:    
         messages.error(request, 'Invalid username or password')
 
-    return redirect(login_url)
+    return redirect(reverse('authors:dashboard'))
 
 @login_required(login_url='authors:login', redirect_field_name='next')
 def logout_view(request):
@@ -81,3 +81,8 @@ def logout_view(request):
     
     logout(request)
     return redirect(reverse('authors:login'))
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_view(request):
+    return render(request, 'authors/pages/dashboard.html')
--- recipes/admin.py
+++ recipes/admin.py
@@ -9,4 +9,13 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-   pass 
+   list_display = [ 'id', 'title', 'created_at', 'is_published', 'author']
+   list_display_links = ['title', 'created_at']
+   search_fields = ['id', 'title', 'description', 'slug', 'preparation_steps']
+   list_filter = ['category', 'author', 'is_published' ]
+   list_per_page = 10
+   list_editable = ['is_published']
+   ordering = '-id'
+   prepopulated_fields = {
+      'slug' : ('title', )
+   }
