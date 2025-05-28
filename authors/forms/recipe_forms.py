87086efce706ef173e93a5fd6997c5827d925e4e
livia.vilhena <liviavilhena@outlook.com>
2025-05-28 14:28:00 -0300

    Criação da verificação de numeros positivos, textos e descrição

--- authors/forms/recipe_forms.py
+++ authors/forms/recipe_forms.py
@@ -1,10 +1,16 @@
-from django import forms
+
 from recipes.models import Recipe
 from utils.django_forms import add_attr
+from collections import defaultdict
+from django.core.exceptions import ValidationError
+
+from utils.string import is_positive_number
 
 class AuthorRecipeForm(forms.ModelForm):
     def __ini__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self._errors = defaultdict(list)
 
         add_attr(self.fields.get('preparation_steps'), 'class' , 'span-2')
         add_attr(self.fields.get('cover'), 'class' , 'span-2')
@@ -33,3 +39,28 @@ class AuthorRecipeForm(forms.ModelForm):
             )
         }
     
+    def clean(self, *args, **kwargs):
+        super_clean = super().clean(*args, **kwargs)
+        cleaned_data= self.cleaned_data
+
+        title =  cleaned_data.get('title')
+        description = cleaned_data.get('description')
+
+        if len(title) < 5:
+            self._errors['title'].append('Must have at least 5 characteres')
+        
+        if title == description:
+            self._errors['title'].append('Cannot be equal to description')
+            self._errors['description'].append('Cannot be equal to title')
+
+
+        if self._errors:
+            raise ValidationError(self._errors)
+
+        return super_clean
+    
+    def clean_preparation_time(self):
+        cleaned_data =  self.cleaned_data.get('preparation_time')
+
+        if not is_positive_number(cleaned_data):
+            self._errors['preparation_time'].append('Must be a positive number') 
