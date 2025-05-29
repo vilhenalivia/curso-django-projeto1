274d4a596ec7313c72bf8e97d73e72104796565b
livia.vilhena <liviavilhena@outlook.com>
2025-05-29 09:59:18 -0300

    Criação da função get_absolute_url para ter o 'ver no site'

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User 
+from django.urls import reverse
 # Create your models here.
 # Cada modulo representa uma tabela no Banco de dados
 class Category(models.Model):
@@ -30,3 +31,7 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('recipes:recipe', args=(self.id))
+    
