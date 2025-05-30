9027bbfc3339de9476a12b3f17d025a3487d788b
livia.vilhena <liviavilhena@outlook.com>
2025-05-30 09:00:21 -0300

    Adicionando select_related()

--- recipes/views.py
+++ recipes/views.py
@@ -27,6 +27,7 @@ class RecipeListViewBase(ListView):
     def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs)
         qs = qs.filter(is_published= True)
+        qs.select_related('author', 'category')
         return qs
 
     # Manipulação de contexto
